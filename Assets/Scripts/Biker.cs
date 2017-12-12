c30ebf3a0fd419d1e2335bd06712fac64a523862
周闽强 <zhouminqiang@kuainiugroup.com>
2017-12-12 19:45:39 +0800

    animation init

---
+++ Assets/Scripts/Biker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Biker : MonoBehaviour {
+
+    private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float v = Input.GetAxisRaw("Vertical");
+        animator.SetInteger("Vertical", (int)v);
+        transform.Translate(Vector3.forward * v * Time.deltaTime * 2);
+
+	}
+}
