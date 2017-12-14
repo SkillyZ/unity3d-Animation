79f002945e5f57d0fc27001d5dc13c711f08e419
周闽强 <zhouminqiang@kuainiugroup.com>
2017-12-14 17:57:28 +0800

    新增跳跃动画

---
+++ Assets/Scripts/FollowTarget.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTarget : MonoBehaviour {
+
+    private Transform target;
+    private Vector3 offset;
+    private int smoothing = 3;
+
+	// Use this for initialization
+	void Start () {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        offset = transform.position - target.position; //局部坐标
+    }
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        //把offset转换为target下的世界坐标
+        Vector3 targetPosition = target.position + target.TransformDirection(offset);
+        transform.position = Vector3.Lerp(transform.position , targetPosition, Time.deltaTime * smoothing);
+        transform.LookAt(target.position);
+	}
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,24 +9,64 @@ public class Player : MonoBehaviour {
     private int SpeedId = Animator.StringToHash("Speed");
     private int IsSpeedUpId = Animator.StringToHash("IsSpeedUp");
     private int HorizontalId = Animator.StringToHash("Horizontal");
+    private int SpeedRotateId = Animator.StringToHash("SpeedRotate");
+    private int SpeedZId = Animator.StringToHash("SpeedZ");
+    private int VaultId = Animator.StringToHash("Vault");
+    private int ColliderId = Animator.StringToHash("Collider");
+
+    private Vector3 matchTarget = Vector3.zero;
+    private CharacterController character;
 
     // Use this for initialization
     void Start () {
         animator = GetComponent<Animator>();
+        character = GetComponent<CharacterController>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        animator.SetFloat(SpeedId, Input.GetAxis("Vertical"));
-        animator.SetFloat(HorizontalId, Input.GetAxis("Horizontal"));
+	void Update ()
+    {
+        animator.SetFloat(SpeedZId, Input.GetAxis("Vertical") * 4.1f);
+        animator.SetFloat(SpeedRotateId, Input.GetAxis("Horizontal") * 126f);
 
-        if (Input.GetKeyDown(KeyCode.LeftShift))
+        bool isVault = false;
+        if (animator.GetFloat(SpeedZId) > 3 && animator.GetCurrentAnimatorStateInfo(0).IsName("Locomotion"))
         {
-            animator.SetBool(IsSpeedUpId, true);
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position + Vector3.up * 0.3f, transform.forward, out hit, 4))
+            {
+                if (hit.collider.tag == "Obstacle")
+                {
+                    if (hit.distance > 3)
+                    {
+                        Vector3 point = hit.point;
+                        point.y = hit.collider.transform.position.y + hit.collider.bounds.size.y + 0.07f;
+                        matchTarget = point;
+                        isVault = true;
+                    }
+                }
+            }
         }
-        if (Input.GetKeyUp(KeyCode.LeftShift))
+        animator.SetBool(VaultId, isVault);
+            
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Vault") && animator.IsInTransition(0) == false)
         {
-            animator.SetBool(IsSpeedUpId, false);
+            animator.MatchTarget(matchTarget, Quaternion.identity, AvatarTarget.LeftHand, new MatchTargetWeightMask(Vector3.one, 0), 0.32f, 0.4f);
         }
-	}
+
+        character.enabled = animator.GetFloat(ColliderId) < 0.5f;
+
+        //animator.SetFloat(SpeedId, Input.GetAxis("Vertical") * 4.1f);
+        //animator.SetFloat(SpeedId, Input.GetAxis("Vertical"));
+        //animator.SetFloat(HorizontalId, Input.GetAxis("Horizontal"));
+
+        //if (Input.GetKeyDown(KeyCode.LeftShift))
+        //{
+        //    animator.SetBool(IsSpeedUpId, true);
+        //} 
+        //if (Input.GetKeyUp(KeyCode.LeftShift))
+        //{
+        //    animator.SetBool(IsSpeedUpId, false);
+        //}
+    }
 }
